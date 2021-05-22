a5a16ade10207e55db09416022b3e748ef9dc5b9
Alok Singh <65681896+Alok-Singh-ops@users.noreply.github.com>
2021-05-22 16:59:14 +0530

    Youtube UI Clone

---
+++ 2. Youtube Clone/app.js
@@ -0,0 +1,19 @@
+// const displayBar = document.getElementById("hamburger");
+
+
+// loadEventListner();
+
+
+
+// function loadEventListner(){
+//   displayBar.addEventListener('click',showBar);
+// }
+
+// function showBar(e) {
+  
+// }
+
+function openNav() {
+  document.getElementById("hiddenBar").style.display = 'block'
+  // document.getElementById("main").style.marginLeft = '250px'
+}
